1844b27a89d59812063cdca8db896bd4097dd707
jido <denisgithub@bredelet.com>
2016-04-20 20:01:56 +0100

    Fix call to undefined function, allow method calls
    For security methodCall is false by default. 
    Also loop over function call for blocks, for when the function returns a function.

--- mistigri.js
+++ mistigri.js
@@ -18,7 +18,13 @@ var mistigri = (function(){
 var DEFAULT_OPEN_BRACE = "{{";
 var DEFAULT_CLOSE_BRACE = "}}";
 var DEFAULT_PLACEHOLDER_TEXT = "N/A";
-var DEFAULT_ESCAPE_FUNCTION = escapeHtml;
+var DEFAULT_METHOD_CALL = false;
+var DEFAULT_ESCAPE_FUNCTION = function escapeHtml(html) {
+    var text = document.createTextNode(html);
+    var div = document.createElement('div');
+    div.appendChild(text);
+    return div.innerHTML;
+}
 
 var main = function prrcess(template, model, config) {
     open_brace = (config && 'openBrace' in config) ? config.openBrace : DEFAULT_OPEN_BRACE;
@@ -30,6 +36,7 @@ var render = function render(parts, model, config) {
     var close_brace = (config && 'closeBrace' in config) ? config.closeBrace : DEFAULT_CLOSE_BRACE;
     var default_text = (config && 'placeholderText' in config) ? config.placeholderText : DEFAULT_PLACEHOLDER_TEXT;
     var default_escape = (config && 'escapeFunction' in config) ? config.escapeFunction : DEFAULT_ESCAPE_FUNCTION;
+    var bind = (config && 'methodCall' in config) ? config.methodCall : DEFAULT_METHOD_CALL;
     var text = parts[0];
     var rendered = text;
     var position = rendered.length;
@@ -53,7 +60,7 @@ var render = function render(parts, model, config) {
                     break;      // break out of switch case
                 }
                 in_block = 1;
-                action = parseAction(mistigri.substr(1), args);
+                action = parseAction(mistigri.substr(1), args, bind);
                 args.$prelude = text;
                 start = partnum;
                 start_text = mitext[1];
@@ -61,7 +68,7 @@ var render = function render(parts, model, config) {
             case "/":
                 --in_block;
                 if (in_block > 0) break;
-                if (in_block < 0 || mistigri.replace(/^\/\s\s*/, "") !== action)
+                if (in_block < 0 || mistigri.replace(/^\/\s*/, "") !== action)
                 {
                     rendered += mistigri;       // invalid close tag
                     break;
@@ -77,13 +84,13 @@ var render = function render(parts, model, config) {
                 break;  // just a comment
             case "&":
                 if (in_block > 0) break;
-                action = parseAction(mistigri.substr(1), args);
-                rendered += handleValue(action, args);
+                action = parseAction(mistigri.substr(1), args, bind);
+                rendered += handleValue(action, args, bind);
                 break;
             default:
                 if (in_block > 0) break;
-                action = parseAction(mistigri, args);
-                rendered += default_escape(handleValue(action, args));
+                action = parseAction(mistigri, args, bind);
+                rendered += default_escape(handleValue(action, args, bind));
         }
         if (in_block <= 0)
         {
@@ -107,18 +114,18 @@ var splitAt = function splitAt(pattern, text) {
     }
 }
 
-var parseAction = function parseAction(tag, args) {
+var parseAction = function parseAction(tag, args, bind) {
     var parts = tag.split(/\s+/);
     var action = parts.shift();
     if (action.length === 0)
     {
         action = parts.shift();
     }
-    getArgs(parts, args);
+    getArgs(parts, args, bind);
     return action;
 }
 
-var getArgs = function getArgs(parts, args) {
+var getArgs = function getArgs(parts, args, bind) {
     var name = "";
     var seen_sign = false;
     var single_quoted = false;
@@ -172,7 +179,7 @@ var getArgs = function getArgs(parts, args) {
                 }
                 else
                 {
-                    arg = valueFor(value, args.$model);
+                    arg = valueFor(value, args.$model, bind);
                 }
                 args[name] = arg;
             }
@@ -186,15 +193,8 @@ var getArgs = function getArgs(parts, args) {
     return !seen_sign;
 }
 
-var escapeHtml = function escapeHtml(html) {
-    var text = document.createTextNode(html);
-    var div = document.createElement('div');
-    div.appendChild(text);
-    return div.innerHTML;
-}
-
-var handleValue = function handleValue(action, args) {
-    var value = valueFor(action, args.$model);
+var handleValue = function handleValue(action, args, bind) {
+    var value = valueFor(action, args.$model, bind);
     var result;
     switch (typeof value) 
     {
@@ -229,8 +229,9 @@ var handleBlock = function handleBlock(action, args, content, parts, config) {
     parts[0] = content;
     args.$invertBlock = invert;
     args.$template = parts;
-    var value = valueFor(action, args.$model);
-    if (typeof value === 'function')
+    var bind = (config && 'methodCall' in config) ? config.methodCall : DEFAULT_METHOD_CALL;
+    var value = valueFor(action, args.$model, bind);
+    while (typeof value === 'function')
     {
         value = value(args);
         if (typeof value === 'string')
@@ -267,7 +268,7 @@ var handleBlock = function handleBlock(action, args, content, parts, config) {
     return result;
 }
 
-var valueFor = function valueFor(name, model) {
+var valueFor = function valueFor(name, model, bind) {
     var path = name.split(".");
     var value = model[path.shift()];
     for (var child in path)
@@ -276,7 +277,13 @@ var valueFor = function valueFor(name, model) {
         {
             break;
         }
-        value = value[path[child]];
+        var next = value[path[child]];
+        if (bind && typeof next === 'function')
+        {
+            var saved = value;
+            next = next.bind(saved);
+        }
+        value = next;
     }
     return value;
 }
