905ae1ea66dd2a3d57f806a28dfcc10b8477a3fd
Denis Bredelet <denisgithub@bredelet.com>
2016-04-29 08:27:04 +0100

    Add Promise shim for systems lacking Promise

---
+++ Promise.js
@@ -0,0 +1,8 @@
+// Minimal promise shim to use with Mistigri if the real deal is not available
+
+function Promise(execute) {
+    this.then = execute;
+    this.catch = function catchNothing(_) {
+        return this;
+    }
+}
