ca7e9ba4d59fc18e33296bdfc5f70d301a9d59b0
jido <denisgithub@bredelet.com>
2016-05-02 18:13:53 +0100

    Providing a readFile function for Mistigri

---
+++ ReadFile.js
@@ -0,0 +1,13 @@
+// A Promise-ready version of fs.readFile to use as reader for Mistigri
+
+var fs = require("fs");
+
+function readFile(name, options) {
+    return new Promise(function fsReadFile(fulfill, reject) {
+        var fsCallback = function fsCallback(error, data) {
+            if (error) reject(error);
+            else fulfill(data);
+        }
+        fs.readFile(name, (options === undefined) ? "utf8" : options, fsCallback);
+    });
+}
