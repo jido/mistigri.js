19a74c2b9ab92e96e16545d5f34ea4f66f2ab83b
jido <denisgithub@bredelet.com>
2016-05-17 07:57:28 +0100

    Revert to filter example with promises. The rawtext example shows how to use Mistigri without promises already. Bump version for npm.

--- Examples/filter.js
+++ Examples/filter.js
@@ -1,21 +1,30 @@
 var mistigri = require("../mistigri.js");
 var fs = require("fs");
 
+// Some generic setup
+
 mistigri.options.escapeFunction = String;
 
-// A filter function which converts to uppercase
+function readFile(name, options) {
+    return new Promise(function fsReadFile(fulfill, reject) {
+        var fsCallback = function fsCallback(error, data) {
+            if (error) reject(error);
+            else fulfill(data);
+        }
+        fs.readFile(name, (options === undefined) ? "utf8" : options, fsCallback);
+    });
+}
+
+// This is the filter function
 
 function uppercaseFilter(args) {
     var toUpper = function toUpper(text) {
-        return String(text).toLocaleUpperCase();
+        return text.toLocaleUpperCase();
     }
     
     if ('$invertBlock' in args) // we are inside a block
     {
-        return toUpper(mistigri.prrcess(args.$template, args.$model));
-        // The above assumes no includes.
-        // If there were includes the function would use promises instead:
-        // return mistigri.prrcess(args.$template, args.$model).then(toUpper);
+        return mistigri.prrcess(args.$template, args.$model).then(toUpper);
     }
     else
     {
@@ -25,9 +34,8 @@ function uppercaseFilter(args) {
 
 // Render the template using the filter
 
-var model = {CAPS: uppercaseFilter};
-
-fs.readFile("filter.mi", "utf8", function(error, template) {
-    if (error) throw error;
-    console.log("" + mistigri.prrcess(template, model));
-});
+readFile("filter.mi").then(function(template) {
+    return mistigri.prrcess(template, {
+        CAPS: uppercaseFilter
+    });
+}).then(console.log);
